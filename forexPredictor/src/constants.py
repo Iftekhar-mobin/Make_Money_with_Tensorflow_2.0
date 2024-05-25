3f60b725a76d7538fc8194229f6e1edd2c835140
iftekhar-mobin <54829611+Iftekhar-mobin@users.noreply.github.com>
2024-05-25 09:49:38 +0600

    kkkkkkk

---
+++ forexPredictor/src/constants.py
@@ -0,0 +1,17 @@
+# login to Trade Account with login()
+# make sure that trade server is enabled in MT5 client terminal
+
+login = 97070354
+password = 'iIeElL0176_md.'
+server = 'XMGlobal-MT5 5'
+
+symbols = ['EURUSD',
+           'GBPUSD',
+           'USDCAD',
+           'USDCHF',
+           'USDJPY',
+           'AUDUSD',
+           ]
+
+start_time = '2015, 1, 1'
+timeframes = ['TIMEFRAME_H1', 'TIMEFRAME_H4']
